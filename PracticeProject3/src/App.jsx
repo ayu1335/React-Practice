3f63e68e304d1fb778176b6e6de89d86c59f72fa
ayu1335 <ayushh1335@proton.me>
2025-04-01 22:16:55 +0530

    add some chages in home,app component

--- PracticeProject3/src/App.jsx
+++ PracticeProject3/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Home from "./components/Home";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link, useLocation } from "react-router-dom";
 import Details from "./components/Details";
 const App = () => {
+  const { search, pathname } = useLocation();
   return (
     <div className="w-screen h-screen flex">
+      {(pathname != "/" || search.length > 0) && (
+        <Link className="text-blue-700 absolute" to="/">
+          home
+        </Link>
+      )}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<Details />} />
--- PracticeProject3/src/components/Home.jsx
+++ PracticeProject3/src/components/Home.jsx
@@ -1,32 +1,52 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Nav from "./Nav";
-import Details from "./Details";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../utils/Context";
 import Loading from "./Loading";
+import instance from "../utils/axios";
 
 const Home = () => {
   const [products] = useContext(ProductContext);
-  console.log(products);
+  const { search } = useLocation();
+  const category = decodeURIComponent(search.split("=")[1]);
+  // let filteredProducts = products && products;
+  const [filteredProducts, setfilteredProducts] = useState(null);
+
+  const getProrductCategory = async () => {
+    try {
+      const { data } = await instance(`/products/category/${category}`);
+      setfilteredProducts(data);
+      console.log(filteredProducts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    if (!filteredProducts || category == "undefined")
+      setfilteredProducts(products);
+    if (category != "undefined") getProrductCategory();
+  }, [category, products]);
+
   return products ? (
     <>
       <Nav />
       <div className="w-[85%] p-10 pt-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto">
-        {products.map((p, i) => (
-          <Link
-            key={p.id}
-            to={`/details/${p.id}`}
-            className="mr-3 mb-3 p-3 w-[18%] h-[30vh] border shadow rounded flex flex-col justify-center items-center"
-          >
-            <div
-              className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
-              style={{
-                backgroundImage: `url(${p.image})`,
-              }}
-            ></div>
-            <h1 className="hover:text-blue-300">{p.title}</h1>
-          </Link>
-        ))}
+        {filteredProducts &&
+          filteredProducts.map((p, i) => (
+            <Link
+              key={i}
+              to={`/details/${p.id}`}
+              className="mr-3 mb-3 p-3 w-[18%] h-[30vh] border shadow rounded flex flex-col justify-center items-center"
+            >
+              <div
+                className="hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center"
+                style={{
+                  backgroundImage: `url(${p.image})`,
+                }}
+              ></div>
+              <h1 className="hover:text-blue-300">{p.title}</h1>
+            </Link>
+          ))}
       </div>
     </>
   ) : (
